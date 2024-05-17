9a70325adb4c30f539235280f401b779745e11a3
Daniel Trifunoski <159186989+stap0t@users.noreply.github.com>
2024-05-17 15:10:55 +0200

    Update property and const_property test.cpp

--- property and const_property test.cpp
+++ property and const_property test.cpp
@@ -4,6 +4,29 @@
 
 /* This was tested on MSVC only and works for C++14, C++17, C++20 standards (haven't tested for other standards */
 
+class Employee
+{
+public:
+	dt0::property<__int64> Id {0, dt0::get<__int64>{}, dt0::set<__int64>{}};
+	dt0::property<std::string> Name {dt0::get<std::string>{}, dt0::set<std::string>{}};
+	dt0::property<std::string> Surname {dt0::get<std::string>{}, dt0::set<std::string>{}};
+	dt0::property<unsigned short> Age {0, dt0::get<unsigned short >{}, dt0::set<unsigned short>{}};
+	dt0::property<unsigned int> Salary {0, dt0::get<unsigned int>{}, dt0::set<unsigned int>{}};
+
+	Employee() noexcept = default;
+
+	Employee(__int64 id, std::string name, std::string surname, unsigned short age, unsigned int salary)
+	{
+		Id = id;
+		Name = name;
+		Surname = surname;
+		Age = age;
+		Salary = salary;
+	}
+
+	~Employee() noexcept = default;
+};
+
 int main()
 {
 	dt0::property<int, int, int&> A { 2, 
@@ -29,5 +52,13 @@ int main()
 	C = 2.71828;
 
 	std::cout << C << "\n\n";
+
+	Employee employee(0xDEEDAA, "John", "Doe", 46, 4000);
+
+	std::cout << "Id: " << employee.Id
+			  << "\nName: " << employee.Name
+			  << "\nSurname: " << employee.Surname
+			  << "\nAge: " << employee.Age
+			  << "\nSalary: " << employee.Salary << "$\n\n";
 	return 0;
 }
