6f4a7a00d44575386a720855f17f29379034cd69
Daniel Trifunoski <159186989+stap0t@users.noreply.github.com>
2024-05-17 03:55:21 +0200

    Update property and const_property test.cpp

--- property and const_property test.cpp
+++ property and const_property test.cpp
@@ -10,7 +10,7 @@ int main()
 		},
 		dt0::set<int>{ 
 			[](int& x, const int& y) {x = 2 * y; }, 
-			[](int& x, int&& y) noexcept {x = std::move(y * 3); 
+			[](int& x, int&& y) noexcept17 {x = std::move(y * 3);
 		}} 
 	};
 
