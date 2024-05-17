fbf9a7a8ae5119c13414ca4a8ffbea8e7114035d
Daniel Trifunoski <159186989+stap0t@users.noreply.github.com>
2024-05-17 03:46:22 +0200

    Create property and const_property test.cpp

---
+++ property and const_property test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "property.hpp"
+
+int main()
+{
+	dt0::property<int, int, int&> A { 2, 
+		dt0::get<int, int, int&> { 
+		[](int& x) -> int { return x * 2; } 
+		},
+		dt0::set<int>{ 
+			[](int& x, const int& y) {x = 2 * y; }, 
+			[](int& x, int&& y) noexcept {x = std::move(y * 3); 
+		}} 
+	};
+
+	std::cout << A << "\n\n";
+
+	dt0::property<std::string> B { dt0::get<std::string>{}, dt0::set<std::string>{} };
+
+	B = "Hello";
+
+	std::cout << B << "\n\n";
+
+	dt0::const_property<double> C{ dt0::get<double>{} };
+
+	C = 2.71828;
+
+	std::cout << C << "\n\n";
+	return 0;
+}
